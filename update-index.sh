bc02435c1d524d542f39e264c5af508a034de8bb
Arne Diekmann <arne@neoskop.de>
2021-01-12 23:52:31 +0100

    chore: migrate to yq v4 syntax

--- update-index.sh
+++ update-index.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-set -e
+set -ef
 git checkout gh-pages
 REPOS="cert-manager-webhook-dnsimple paperboy papergirl mgnl-on-k8s"
-mkdir .cr-release-packages
 
 for repo in $REPOS ; do
   mkdir $repo
   wget -O ./$repo/index.yaml https://raw.githubusercontent.com/neoskop/$repo/gh-pages/index.yaml
 done
 
-yq m `for repo in $REPOS ; do echo $repo/index.yaml ; done` | yq w - 'generated' `date -Ins | tr , .` > index.yaml
-rm -rf .cr-release-packages
+REPOS_ARR=( $REPOS )
+YQ_EXPR=""
+
+for ((i=0; i<${#REPOS_ARR[@]}; i++)) ; do 
+  YQ_EXPR="${YQ_EXPR}select(fi == $i)"
+  
+  if [ "$i" -ne "$((${#REPOS_ARR[@]} - 1))" ]; then
+    YQ_EXPR="${YQ_EXPR} * " 
+  fi
+done
+
+yq eval-all "$YQ_EXPR" `for repo in $REPOS ; do echo $repo/index.yaml ; done` | yq eval ".generated=\"`date -Ins | tr , .`\"" - > index.yaml
 for repo in $REPOS ; do rm -rf $repo ; done
 git add index.yaml
 git commit -m "chore: Update chart index."
